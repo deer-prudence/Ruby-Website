03cee16de497da5e57adf93f846d13a6402e5e35
Ubuntu <ubuntu@ip-172-31-2-238.us-east-2.compute.internal>
2020-11-10 18:59:53 +0000

    added donatella page and page navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-    def hello
-        render html: "¡Hola, mundo!"
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/work'
   get 'static_pages/contact'
+  get 'static_pages/donatella'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
